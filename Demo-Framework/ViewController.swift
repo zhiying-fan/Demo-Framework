569924ecab0fca0e5e274d3e3eee635c0b26a3a1
Zhiying Fan <zhiying.fan@rea-group.com>
2021-02-04 15:22:00 +0800

    Import system dynamic framework

--- Demo-Framework/ViewController.swift
+++ Demo-Framework/ViewController.swift
@@ -6,14 +6,8 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
-  }
-
-
+  @IBOutlet weak var appleMapView: MKMapView!
 }
-
